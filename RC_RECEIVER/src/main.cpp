baa25aed507daac418cbb38729e06f55109fa563
LITHIUM03 <53534129+LITHIUM03@users.noreply.github.com>
2020-12-04 11:11:34 +0200

    Button and led version. works, but led still blinks rapidly. probably a code problem.

--- RC_RECEIVER/src/main.cpp
+++ RC_RECEIVER/src/main.cpp
@@ -2,7 +2,9 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define LED_PIN 3
 RF24 radio(7, 8); // CE, CSN
+int msg[1];
 
 const byte address[6] = "00001";
 
@@ -12,12 +14,35 @@ void setup() {
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+  pinMode(LED_PIN,OUTPUT);
 }
 
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+
+
+
+void loop(void){
+// bool done = false;
+
+if (radio.available()){
+  radio.read(msg, 1);
+  Serial.println(msg[0]);
+
+  if (msg[0] == 111){
+    //delay(10);
+    digitalWrite(LED_PIN, HIGH);
+    }
+  else {
+    digitalWrite(LED_PIN, LOW);
+    //delay(10);
+    }
 }
+else{
+    Serial.println("No radio available");
+    digitalWrite(LED_PIN, LOW);
+    //delay(10);
+}
+  
+delay(10);}
+
+
+  
--- RC_control/src/main.cpp
+++ RC_control/src/main.cpp
@@ -2,19 +2,33 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define BUTTON_PIN 2 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
+int msg[1];
+
 void setup() {
+  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  pinMode(BUTTON_PIN,INPUT_PULLUP);
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+void loop(){
+
+if (digitalRead(BUTTON_PIN) == LOW){
+msg[0] = 111;
+radio.write(msg, 1);
+Serial.print("inside tx IF\n");
+}
+else{
+msg[0] = 000;
+radio.write(msg, 1);
+Serial.print("inside tx loop, not IF\n");
+}
+//delay(50);
 }
